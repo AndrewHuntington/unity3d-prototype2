342ca40a4f5c754e36ec66909c73e0f9da409774
Andrew Huntington <andrew.huntington@gmail.com>
2023-12-05 09:57:45 +0900

    Finish Challenge 2 - Bonus Challaenges

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public GameObject dogPrefab;
+  public GameObject dogPrefab;
+  private float timeSinceLastInstantiation = 0f;
+  public float instantiationDelay = 0.5f;
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    // Update the timer
+    timeSinceLastInstantiation += Time.deltaTime;
+
+    // Check if spacebar is pressed and if the delay has passed
+    // If so, send dog
+    if (Input.GetKeyDown(KeyCode.Space) && timeSinceLastInstantiation >= instantiationDelay)
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-        }
+      Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+      // Reset the timer after instantiation
+      timeSinceLastInstantiation = 0f;
     }
+
+
+  }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -10,13 +10,22 @@ public class SpawnManagerX : MonoBehaviour
   private float spawnLimitXRight = 7;
   private float spawnPosY = 30;
 
-  private float startDelay = 1.0f;
-  private float spawnInterval = 4.0f;
-
   // Start is called before the first frame update
   void Start()
   {
-    InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+    // Start the spawning process
+    StartCoroutine(SpawnRandomBallWithDelay());
+  }
+  IEnumerator SpawnRandomBallWithDelay()
+  {
+    while (true)
+    {
+      float delay = Random.Range(3f, 5f);
+      Debug.Log(delay);
+      yield return new WaitForSeconds(delay);
+
+      SpawnRandomBall();
+    }
   }
 
   // Spawn random ball at random x position at top of play area
@@ -29,5 +38,4 @@ public class SpawnManagerX : MonoBehaviour
     // instantiate ball at random spawn location
     Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
   }
-
 }
