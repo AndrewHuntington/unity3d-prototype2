ca7584284d0db87626d9b68a2129ede46529d010
Andrew Huntington <andrew.huntington@gmail.com>
2023-12-06 22:40:37 +0900

    Give player vertical movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,37 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalInput;
-    public float speed = 10.0f;
-    public float xRange = 10.0f;
+  public float horizontalInput;
+  public float verticalInput;
+  public float speed = 10.0f;
+  public float xRange = 15.0f;
+  public float zRange = 6.0f;
 
-    public GameObject projectilePrefab;
+  public GameObject projectilePrefab;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    // Keep the player in bounds
+    if (transform.position.x < -xRange)
     {
-        
+      transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+    }
+    if (transform.position.x > xRange)
+    {
+      transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+    }
+    if (transform.position.z < 0)
+    {
+      transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+    }
+    if (transform.position.z > zRange)
+    {
+      transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Keep the player in bounds
-        if (transform.position.x < -xRange)
-        {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }
-        
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
+    horizontalInput = Input.GetAxis("Horizontal");
+    transform.Translate(horizontalInput * speed * Time.deltaTime * Vector3.right);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
+    verticalInput = Input.GetAxis("Vertical");
+    transform.Translate(verticalInput * speed * Time.deltaTime * Vector3.forward);
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
     }
+  }
 }
